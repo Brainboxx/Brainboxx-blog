9315acd4b3ece2d4d0e4c4051076bdc49e410dd8
user <chineduudochuku66@gmail.com>
2022-10-21 18:06:01 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app.config['SECRET_KEY'] = secret_key
 Bootstrap(app)
 print(app.config['SECRET_KEY'])
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
